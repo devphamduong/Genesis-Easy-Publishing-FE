96658ea4b2101ae4f00ddce1f9a0761e4943a45c
DuongPC <pduong244@gmail.com>
2024-01-26 00:16:35 +0700

    config deploy 04

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Genesis-Easy-Publishing-FE/",
+  base: "/Genesis-Easy-Publishing-FE/dist/",
   plugins: [react()],
   server: {
     port: 3000,

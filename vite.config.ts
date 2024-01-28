b3f963ff6d14870f47e5dec3056c8d1a180daa08
Pham Chu Duong <116943699+devphamduong@users.noreply.github.com>
2024-01-28 22:06:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist/",
+  base: "/Genesis-Easy-Publishing-FE/dist/",
   plugins: [react()],
   server: {
     port: 3000,
